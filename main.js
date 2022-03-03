bab4d0d546cf84bcde09e5201c13fc790ba3c038
ji <jiyongprk@gmail.com>
2022-03-03 14:33:40 -0500

    work on more javascript

--- main.js
+++ main.js
@@ -14,5 +14,46 @@ const imageArray = [{
     id : 3,
     src: "images/pexels-pixabay-273935.jpg" 
 }];
+const images = document.querySelector('img');
+const prevBtns = document.querySelector('.prev-btn');
+const nextBtns = document.querySelector('.next-btn');
+
+
+let slideNum = 0;
+console.log(slideNum);
+
+function nextBtn () {
+    nextBtns.addEventListener('click',()=>{
+        slideNum++;
+        console.log(slideNum);
+        if (slideNum >= 4) {
+            slideNum = 0;
+        }
+        showCase(slideNum);
+    })
+}
+nextBtn();
+
+// function prevBtn (){
+//     prevBtns.addEventListener('click',()=>{
+//         slideNum--;
+//         console.log(slideNum);
+//     })
+// }
+// prevBtn();
+
+
+function showCase(slideNum) {
+    let imageSource = imageArray[slideNum]
+    console.log(imageSource);
+    images.src = imageSource.src;
+}
+
+
+// window.addEventListener('DOMContentLoaded',()=>{
+//     images.src = imageSource.src[0];
+// });
+
+
 
 
