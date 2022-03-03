0c3c78e3da95656926dd30ccdf3fef8e11e5b0dc
ji <jiyongprk@gmail.com>
2022-03-03 13:54:32 -0500

    worked on html,css,javascript

---
+++ main.js
@@ -0,0 +1,18 @@
+const imageArray = [{
+    id : 0,
+    src: "images/pexels-andy-vu-3484061.jpg" 
+},
+{
+    id : 1,
+    src: "images/pexels-roberto-nickson-2775196.jpg" 
+},
+{
+    id : 2,
+    src: "images/pexels-lumn-167699.jpg" 
+},
+{
+    id : 3,
+    src: "images/pexels-pixabay-273935.jpg" 
+}];
+
+
